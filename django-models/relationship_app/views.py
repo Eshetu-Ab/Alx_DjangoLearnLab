328f727aaf43ac4b7b55ae1fb37bc943a2a726ba
Eshetu-Ab <eshetu190@gmail.com>
2024-08-18 14:09:39 +0300

    relationship_app added

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,7 +6,6 @@ from .models import Book, Library
 
 def list_books(request):
     books = Book.objects.all()
-    # Here, we're referencing the 'list_books.html' template
     return render(request, 'list_books.html', {'books': books})
 
 # Class-Based View
