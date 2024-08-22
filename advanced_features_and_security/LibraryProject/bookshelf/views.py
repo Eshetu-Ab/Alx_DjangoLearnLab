80d0c63c677d709cdffb195ad4350aba9b5ad890
Eshetu-Ab <eshetu190@gmail.com>
2024-08-22 13:26:52 +0300

    CSRF protection enabled for all forms

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -38,3 +38,30 @@ def secure_view(request):
     response = HttpResponse("Secure content")
     response['Content-Security-Policy'] = "default-src 'self';"
     return response
+from django.shortcuts import render, redirect
+from .forms import BookForm
+from .models import Book
+from django.contrib.auth.decorators import permission_required
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'form_example.html', {'form': form})
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'form_example.html', {'form': form})
