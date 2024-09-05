61c32feb68263a54a00b9fa7e39bfdf031ba8503
Eshetu-Ab <eshetu190@gmail.com>
2024-09-05 13:48:42 +0300

    Setting Up a New Django Project with Custom Serializers in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from .models import Book
-from .serializers import BookSerializer
+from .seriealizers import BookSerializer
 from rest_framework import permissions
 
 class BookListView(generics.ListCreateAPIView):
