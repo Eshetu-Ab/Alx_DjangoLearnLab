5fc0226e940e9f7b5d15c6a934a31c7fc67c4eb3
Eshetu-Ab <eshetu190@gmail.com>
2024-09-05 13:57:56 +0300

    Setting Up a New Django Project with Custom Serializers in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,28 +1,33 @@
+# api/views.py
+
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Book
 from .seriealizers import BookSerializer
-from rest_framework import permissions
+from .permissions import IsAdminUser
 
 class BookListView(generics.ListCreateAPIView):
     """
-    View to list all books or create a new book.
+    Retrieve all books or create a new book.
     - GET: List all books.
     - POST: Create a new book.
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminUser]  # Example of using custom permission
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
-    View to retrieve, update, or delete a book instance.
+    Retrieve, update, or delete a specific book by ID.
     - GET: Retrieve a book.
     - PUT/PATCH: Update a book.
     - DELETE: Delete a book.
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
 
 
 
