d03ae23ba0029d4c35c5b4771a9e995b8605c76b
Eshetu-Ab <eshetu190@gmail.com>
2024-09-07 11:52:56 +0300

    Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/advanced_api_project/api/views.py
+++ advanced-api-project/advanced_api_project/api/views.py
@@ -3,32 +3,27 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# ListView for retrieving all books
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-# DetailView for retrieving a single book by ID
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-# CreateView for adding a new book
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]  # Only authenticated users can create books
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
-# UpdateView for modifying an existing book
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]  # Only authenticated users can update books
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
-# DeleteView for removing a book
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]  # Only authenticated users can delete books
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
