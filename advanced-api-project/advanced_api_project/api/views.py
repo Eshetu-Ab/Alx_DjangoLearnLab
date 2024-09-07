e91d90ddba41c442f6aa7f70d1d07bbe278b0f1e
Eshetu-Ab <eshetu190@gmail.com>
2024-09-07 12:22:34 +0300

    Implementing Filtering, Searching, and Ordering in Django REST Framework

--- advanced-api-project/advanced_api_project/api/views.py
+++ advanced-api-project/advanced_api_project/api/views.py
@@ -1,5 +1,5 @@
 from django_filters import rest_framework as filters
-from rest_framework import generics, filters as drf_filters
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -11,7 +11,7 @@ class BookListView(generics.ListAPIView):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     
- # Add filter, search, and ordering capabilities
+    # Add filter, search, and ordering capabilities
     filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
     filterset_fields = ['title', 'author', 'publication_year']
     search_fields = ['title', 'author__name']
