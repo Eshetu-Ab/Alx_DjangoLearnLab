bbd1e53b2038fbc3c2a244e204d21501814fed7d
Eshetu-Ab <eshetu190@gmail.com>
2024-09-09 10:05:28 +0300

    Task 1: Implementing the Blog's User Authentication System

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BlogDB',
+        'USER': 'root',
+        'PASSWORD': 'Ea&Em@2012EC',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
