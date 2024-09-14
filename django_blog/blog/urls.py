97973b09e68d78d254454b2d8b95dcc880faaeb7
Eshetu-Ab <eshetu190@gmail.com>
2024-09-14 11:30:19 +0300

    Implementing the Blog's User Authentication System

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,10 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # This is for the home page
-    path('posts/', views.posts, name='posts'),  # This is for the posts page
     path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
-    path('profile/', views.profile, name='profile'),
+    # other authentication URLs
 ]
+
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,50 +1,15 @@
 # blog/views.py
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from .forms import CustomUserCreationForm
-from .models import Post
-
-
-def home(request):
-    return render(request, 'home.html')
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
 def register(request):
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'registration/register.html', {'form': form})
-
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+            user = form.save()
             login(request, user)
-            return redirect('home')
-    return render(request, 'registration/login.html')
-
-def user_logout(request):
-    logout(request)
-    return redirect('home')
-
-
-def posts(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/posts.html', {'posts': posts})
-
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        user = request.user
-        user.email = request.POST['email']
-        user.save()
-        return redirect('profile')
-    return render(request, 'registration/profile.html')
-
+            return redirect('profile')  # Redirect to the profile page or home after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
