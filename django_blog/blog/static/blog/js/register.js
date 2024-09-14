16671d5f311eb3b0fa67d91865ec956c42ad0986
Eshetu-Ab <eshetu190@gmail.com>
2024-09-14 11:52:07 +0300

    Implementing the Blog's User Authentication System

--- django_blog/blog/static/blog/js/register.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', function(event) {
-        alert('Registration submitted successfully!');
-    });
-});
--- django_blog/staticfiles/blog/js/login.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', function(event) {
-        alert('Login submitted successfully!');
-    });
-});
