162898f1c4948de455935995a3981d9c1929d83a
Eshetu-Ab <eshetu190@gmail.com>
2024-09-21 11:03:06 +0300

    0. Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,17 +5,22 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
 
+
+
+
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()  # Dynamically get the User model
         fields = ('username', 'email', 'password')
 
     def create(self, validated_data):
-        user = User(**validated_data)
-        user.set_password(validated_data['password'])  # Hash the password
-        user.save()
-        Token.objects.create(user=user)  # Create a token for the new user
+        user = get_user_model().objects.create_user(  # Create a user
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
         return user
+
 
